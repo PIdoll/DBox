4368cf55e714f90460ddc066ce98f512505a64a9
王云辉 <wangyunhui@wangyunhuideMacBook-Pro.local>
2018-11-08 21:30:36 +0800

    UI 修改完毕

--- src/templates/switch.jsx
+++ src/templates/switch.jsx
@@ -16,22 +16,26 @@ export default class SwitchView extends React.Component {
     return (
       <div id='main-container'>
         <h1 className='h1'>基本用法</h1>
+        <p>直接引用组件默认选中属性为defaultChecked</p>
         <Switch defaultChecked />
         <h1 className='h1'>按钮操作</h1>
+        <p>通过操作按钮切换开关的禁用状态</p>
         <Switch disabled={this.state.disabled} />
         <br />
         <Button onClick={this.toggle} >改变禁用状态</Button>
         <h1 className='h1'>两种大小</h1>
+        <p>通过添加属性size为small来改变开关大小，不传为默认</p>
         <Switch />
         <br />
         <Switch size='small' />
         <h1 className='h1'>带有文字的按钮</h1>
+        <p>可通过checkedChildren和unCheckedChildren来设置开关内的文字，图标等</p>
         <Switch checkedChildren={'1'} unCheckedChildren={'0'} />
-        <br />
         <h1 className='h1'>加载中</h1>
-        <Switch loading defaultChecked />
+        <p>通过添加loading属性来改变开关样式</p>
+        <Switch loading disabled defaultChecked />
         <br />
-        <Switch size='small' loading />
+        <Switch disabled size='small' loading />
       </div>
     )
   }
