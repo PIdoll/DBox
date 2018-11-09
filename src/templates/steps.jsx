41fa19680dc36deabb200b39ae67ed70fba83e0d
zhangbin <zhgbin005@gmail.com>
2018-11-09 09:44:45 +0800

    fix the steps func and class

--- src/templates/steps.jsx
+++ src/templates/steps.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import Steps from 'components/steps';
+import Icon from 'components/icon'
+
 const Step = Steps.Step;
 
 export default class StepsView extends React.Component {
@@ -9,15 +11,9 @@ export default class StepsView extends React.Component {
       <div id='main-container'>
         <h1 className='h1'>通用步骤条</h1>
         <Steps current={0}>
-          <Step title='Finished' description='this is a description' />
+          <Step title='Finished' description='this is a description' icon={<Icon type='check-circle' />} />
           <Step title='In Progress' description='this is a description' />
           <Step title='Waiting' description='this is a description' />
-        </Steps>
-        <h1 className='h1'>点状步骤条</h1>
-        <Steps progressDot current={1}>
-          <Step title='Finished' description='This is a description' />
-          <Step title='In Progress' description='This is a description' />
-          <Step title='Waiting' description='This is a description' />
         </Steps>
         <h1 className='h1'>小尺寸步骤条</h1>
         <Steps size='small' >
@@ -25,6 +21,13 @@ export default class StepsView extends React.Component {
           <Step title='In Progress' description='This is a description' />
           <Step title='Waiting' description='This is a description' />
         </Steps>
+        <h1 className='h1'>点状步骤条</h1>
+        <Steps progressDot current={1}>
+          <Step title='Finished' description='This is a description' />
+          <Step title='In Progress' description='This is a description' />
+          <Step title='Waiting' description='This is a description' />
+        </Steps>
+
         <h1 className='h1'>步骤运行错误</h1>
         <Steps current={1} status='error'>
           <Step title='Finished' description='This is a description' />
