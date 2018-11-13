b32b18ec15e7e529c89c3192cd1f9dfab7394f62
hui5078 <839265799@qq.com>
2018-11-13 11:09:00 +0800

    dropdown update

--- components/dropdown/dropdown.jsx
+++ components/dropdown/dropdown.jsx
@@ -4,15 +4,15 @@ import './style/index.jsx'
 
 export default class Dropdown extends React.Component {
   static defaultProps = {
-    transitionName: 'antSlideUpIn',
+    transitionName: 'idollSlideUpIn',
     prefixCls: 'idoll-dropdown',
-    mouseEnterDelay: 0.3,
+    mouseEnterDelay: 0.2,
     mouseLeaveDelay: 0.1,
   }
 
   render() {
     return (
-      <RcDropdown transitionName='antSlideDownOut' {...this.props} />
+      <RcDropdown {...this.props} />
     )
   }
 }
