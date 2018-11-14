1ee06ef27805f4c9a57cd1554a6732560390460b
cxm <18255117203@163.com>
2018-11-14 23:28:28 +0800

    select样式

--- src/templates/select.jsx
+++ src/templates/select.jsx
@@ -1,102 +1,138 @@
 import React from 'react'
 
 import Select from 'components/select'
+import Spin from 'components/spin'
 
-const Option = Select.Option;
+const {Option, OptGroup} = Select;
 
-function handleChange(value) {
-  console.log(value); // { key: "lucy", label: "Lucy (101)" }
+class select extends React.Component {
+  constructor(props) {
+    super(props);
+    this.lastFetchId = 0;
+  }
+
+  state = {
+    data: [],
+    value: [],
+    fetching: false,
+  }
+
+  getUserInfo = (value) => {
+    console.log('getUserInfo ', value);
+    this.setState({ data: [], fetching: true });
+    setTimeout(() => {
+      const data = [
+        {value: '1', text: 'aa'},
+        {value: '2', text: 'bb'},
+        {value: '3', text: 'cc'},
+      ]
+      this.setState({ data, fetching: false });
+    }, 1000);
+  }
+
+  handleChange = (value) => {
+    console.log(value); // { key: "lucy", label: "Lucy (101)" }
+  }
+
+  handleChange2 = (value) => {
+    this.setState({
+      value,
+      data: [],
+      fetching: false,
+    });
+  }
+
+  render() {
+    const { fetching, data, value } = this.state;
+    return (
+      <div id='main-container'>
+        <h1 className='h1'>基本使用</h1>
+        <Select size='small' defaultValue='select1' style={{ width: 200 }}>
+          <Option value='select1'>请选择1</Option>
+          <Option value='select2'>请选择2</Option>
+        </Select>
+        <br /><br />
+
+        <Select defaultValue='select1' style={{ width: 200 }}>
+          <Option value='select1'>请选择1</Option>
+          <Option value='select2'>请选择2</Option>
+        </Select>
+        <br /><br />
+
+        <Select size='large' defaultValue='select1' style={{ width: 200 }}>
+          <Option value='select1'>请选择1</Option>
+          <Option value='select2'>请选择2</Option>
+        </Select>
+        <br /><br />
+
+        <Select defaultValue='select' style={{ width: 200 }} disabled>
+          <Option value='select'>请选择</Option>
+        </Select>
+        <br /><br />
+
+        <h1 className='h1'>搜索下拉框</h1>
+        <Select showSearch style={{ width: 200 }} placeholder='请选择' >
+          <Option value='请选择1'>请选择1</Option>
+          <Option value='请选择2' >请选择2</Option>
+        </Select>
+        <br /><br />
+
+        <h1 className='h1'>多选下拉框</h1>
+        <Select mode='multiple' style={{ width: 200 }} placeholder='多选' >
+          <Option value='select1'>请选择1</Option>
+          <Option value='select2'>请选择2</Option>
+          <Option value='select3' >请选择3</Option>
+        </Select>
+
+        <br /><br />
+        <Select mode='tags' style={{ width: 200 }} placeholder='可随意输入内容' >
+          <Option value='select1'>请选择1</Option>
+          <Option value='select2'>请选择2</Option>
+          <Option value='select3' >请选择3</Option>
+        </Select>
+
+        <br /><br />
+        <h1 className='h1'>获得选项文本</h1>
+        <Select labelInValue defaultValue={{ key: 'select2' }} style={{ width: 200 }} onChange={this.handleChange}>
+          <Option value='select1'>请选择1</Option>
+          <Option value='select2'>请选择2</Option>
+        </Select>
+
+        <br /><br />
+        <h1 className='h1'>搜索用户</h1>
+        <Select
+          mode='multiple'
+          labelInValue
+          value={value}
+          placeholder='Select users'
+          notFoundContent={fetching ? <Spin size='small' /> : null}
+          filterOption={false}
+          onSearch={this.getUserInfo}
+          onChange={this.handleChange2}
+          style={{ width: '20%' }}
+        >
+          {data.map(d => <Option key={d.value}>{d.text}</Option>)}
+        </Select>
+
+        <br /><br />
+        <h1 className='h1'>分组</h1>
+        <Select
+          defaultValue='hefei'
+          style={{ width: 200 }}
+          onChange={this.handleChange}
+        >
+          <OptGroup label='安徽'>
+            <Option value='xuancheng'>宣城</Option>
+            <Option value='hefei'>合肥</Option>
+          </OptGroup>
+          <OptGroup label='江苏'>
+            <Option value='nanjing'>南京</Option>
+            <Option value='suzhou'>苏州</Option>
+          </OptGroup>
+        </Select>
+      </div>
+    )
+  }
 }
-
-// function fetchUser = (value) => {
-//   console.log('fetching user', value);
-//   this.lastFetchId += 1;
-//   const fetchId = this.lastFetchId;
-//   this.setState({ data: [], fetching: true });
-//   fetch('https://randomuser.me/api/?results=5')
-//     .then(response => response.json())
-//     .then((body) => {
-//       if (fetchId !== this.lastFetchId) { // for fetch callback order
-//         return;
-//       }
-//       const data = body.results.map(user => ({
-//         text: `${user.name.first} ${user.name.last}`,
-//         value: user.login.username,
-//       }));
-//       this.setState({ data, fetching: false });
-//     });
-// }
-
-const select = () => (
-  <div id='main-container'>
-    <h1 className='h1'>基本使用</h1>
-    <Select size='small' defaultValue='select1' style={{ width: 160 }}>
-      <Option value='select1'>请选则1</Option>
-      <Option value='select2'>请选则2</Option>
-    </Select>
-    <br /><br />
-
-    <Select defaultValue='select1' style={{ width: 160 }}>
-      <Option value='select1'>请选则1</Option>
-      <Option value='select2'>请选则2</Option>
-    </Select>
-    <br /><br />
-
-    <Select size='large' defaultValue='select1' style={{ width: 160 }}>
-      <Option value='select1'>请选则1</Option>
-      <Option value='select2'>请选则2</Option>
-    </Select>
-    <br /><br />
-
-    <Select defaultValue='select' style={{ width: 160 }} disabled>
-      <Option value='select'>请选则</Option>
-    </Select>
-    <br /><br />
-
-    <h1 className='h1'>搜索下拉框</h1>
-    <Select showSearch style={{ width: 160 }} placeholder='请选择' >
-      <Option value='select1'>请选则1</Option>
-      <Option value='select2' >请选则2</Option>
-    </Select>
-    <br /><br />
-
-    <h1 className='h1'>多选下拉框</h1>
-    <Select mode='multiple' style={{ width: 160 }} placeholder='多选' >
-      <Option value='select1'>请选则1</Option>
-      <Option value='select2'>请选则2</Option>
-      <Option value='select3' >请选则3</Option>
-    </Select>
-
-    <br /><br />
-    <Select mode='tags' style={{ width: 160 }} placeholder='可随意输入内容' >
-      <Option value='select1'>请选则1</Option>
-      <Option value='select2'>请选则2</Option>
-      <Option value='select3' >请选则3</Option>
-    </Select>
-
-    <br /><br />
-    <h1 className='h1'>获得选项文本</h1>
-    <Select labelInValue defaultValue={{ key: 'select2' }} style={{ width: 160 }} onChange={handleChange}>
-      <Option value='select1'>请选则1</Option>
-      <Option value='select2'>请选则2</Option>
-    </Select>
-
-    <br /><br />
-    <h1 className='h1'>搜索用户</h1>
-    {/* <Select
-      mode='multiple'
-      labelInValue
-      value={value}
-      placeholder='Select users'
-      notFoundContent={fetching ? <Spin size='small' /> : null}
-      filterOption={false}
-      onSearch={this.fetchUser}
-      onChange={this.handleChange}
-      style={{ width: '100%' }}
-      >
-      {data.map(d => <Option key={d.value}>{d.text}</Option>)}
-    </Select> */}
-  </div>
-)
 
 export default select;
