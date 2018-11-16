a165ae00688928e0514ea8de361de4ed3b654939
575001446@qq.com <575001446@qq.com>
2018-11-16 08:46:50 +0800

    完成dividerAPI以及样式的优化

--- src/templates/divider.jsx
+++ src/templates/divider.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import Divider from 'components/divider';
 
-const text = `默认为水平分割线，可在中间加入文字。`;
+const text = ``;
 
 export default () => (
   <div id='main-container'>
     <h1 className='h1'>水平分割线</h1>
     <div>
-      <p>{text}</p>
-      <Divider>分割线</Divider>
-      <p>{text}</p>
+      <p>默认为水平分割线(实线)</p>
+      <Divider />
+      <p>默认为水平分割线(虚线)</p>
       <Divider dashed />
-      <p>{text}</p>
-    </div>,
+    </div>
     <h1 className='h1'>竖分割线</h1>
     <div>
     测试
@@ -20,7 +19,7 @@ export default () => (
       <a href='#'>链接</a>
       <Divider type='vertical' />
       <a href='#'>链接</a>
-    </div>,
+    </div>
   </div>
 )
 
