d8439b7e14d18b573193baef05d89d6c7a393fe1
575001446@qq.com <575001446@qq.com>
2018-11-16 08:18:36 +0800

    优化分割线

--- src/templates/divider.jsx
+++ src/templates/divider.jsx
@@ -8,21 +8,19 @@ export default () => (
     <h1 className='h1'>水平分割线</h1>
     <div>
       <p>{text}</p>
-      <Divider />
-      <p>{text}</p>
-      <Divider>水平分割线</Divider>
+      <Divider>分割线</Divider>
       <p>{text}</p>
       <Divider dashed />
       <p>{text}</p>
-    </div>
-    <h1 className='h1'>垂直分割线</h1>
+    </div>,
+    <h1 className='h1'>竖分割线</h1>
     <div>
-      垂直分割线
+    测试
       <Divider type='vertical' />
       <a href='#'>链接</a>
       <Divider type='vertical' />
       <a href='#'>链接</a>
-    </div>
+    </div>,
   </div>
 )
 
