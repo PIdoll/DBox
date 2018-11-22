c6abea2296d8887da1c59cceb0fa83c92ed129c4
cxm <18255117203@163.com>
2018-11-22 14:15:32 +0800

    autocomplete优化

--- src/templates/auto-complete.jsx
+++ src/templates/auto-complete.jsx
@@ -3,30 +3,76 @@ import AutoComplete from 'components/auto-complete/index';
 import Input from 'components/input/index';
 import Icon from 'components/icon/index';
 
+
 const Option = AutoComplete.Option;
 const OptGroup = AutoComplete.OptGroup;
 
 export default class Complete extends React.Component {
   state = {
-    result: [],
+    dataSource: [],
+    dataSource3: ['shanghai', 'BeiJing', 'Shen zhen'],
+    dataSource4: [],
   }
 
   handleSearch = (value) => {
-    let result;
+    let {dataSource} = this.state;
     if (!value || value.indexOf('@') >= 0) {
-      result = [];
+      dataSource = [];
     } else {
-      result = ['gmail.com', '163.com', 'qq.com'].map(domain => `${value}@${domain}`);
+      dataSource = ['126.com', '163.com', 'qq.com'].map(domain => `${value}@${domain}`);
     }
-    this.setState({ result });
+    this.setState({ dataSource });
   }
 
-  render() {
-    const { result } = this.state;
-    const children = result.map((email) => {
-      return <Option style={{ width: 240 }} key={email}>{email}</Option>;
+  onSelect = (value) => {
+    console.log('onSelect', value);
+  }
+
+  getRandomInt = (max, min = 0) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min; // eslint-disable-line no-mixed-operators
+  }
+
+   searchResult = (query) => {
+    return (new Array(this.getRandomInt(5))).join('.').split('.')
+      .map((item, idx) => ({
+        query,
+        category: `${query}${idx}`,
+        count: this.getRandomInt(200, 100),
+      }));
+  }
+
+  renderOption = (item) => {
+    return (
+      <Option key={item.category} text={item.category}>
+        {item.query} 在
+        <a
+          href={`https://s.taobao.com/search?q=${item.query}`}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          {item.category}
+        </a>
+        区块中
+        <span className='global-search-item-count'>约 {item.count} 个结果</span>
+      </Option>
+    );
+  }
+
+  handleSearch2 = (value) => {
+    this.setState({
+      dataSource4: value ? this.searchResult(value) : [],
     });
-    const dataSource = ['Listen', 'Time'];
+  }
+
+
+
+
+  render() {
+    const { dataSource, dataSource3, dataSource4 } = this.state;
+    const children = dataSource.map((email) => {
+      return <Option key={email}>{email}</Option>;
+    });
+    // const dataSource = ['Listen', 'Time'];
     const dataSource2 = [{
       title: '语言',
       children: [{
@@ -89,32 +135,58 @@ export default class Complete extends React.Component {
       </Option>,
     ]);
 
+
     return (
       <div id='main-container'>
+
+        <h1 className='h1'>基本使用</h1>
+        <AutoComplete
+          dataSource={dataSource}
+          style={{ width: 200 }}
+          onSelect={this.onSelect}
+          onSearch={this.handleSearch}
+          placeholder='请输入'
+            />
+
         <h1 className='h1'>自定义选项</h1>
         <AutoComplete
           style={{ width: 200 }}
           onSearch={this.handleSearch}
-          placeholder='input here'
+          placeholder='请输入'
         >
           {children}
         </AutoComplete>
         <h1 className='h1'>不区分大小写</h1>
         <AutoComplete
           style={{ width: 200 }}
-          dataSource={dataSource}
-          placeholder='try to type `t`'
+          dataSource={dataSource3}
+          placeholder='输入小写 `b`'
           filterOption={(inputValue, option) => option.props.children.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1}
         />
-        <h1 className='h1'>查询模式</h1>
+        <h1 className='h1'>查询模式-确定类目</h1>
         <AutoComplete
           style={{ width: '300px' }}
           dataSource={options}
-          placeholder='input here'
+          placeholder='请输入'
           optionLabelProp='value'
         >
           <Input suffix={<Icon type='search' />} />
         </AutoComplete>
+
+        <h1 className='h1'>查询模式-不确定类目</h1>
+        <div className='global-search-wrapper' style={{ width: 300 }}>
+          <AutoComplete
+            className='global-search'
+            style={{ width: '100%' }}
+            dataSource={dataSource4.map(this.renderOption)}
+            onSelect={this.onSelect}
+            onSearch={this.handleSearch2}
+            placeholder='请输入'
+            optionLabelProp='text'
+        >
+            <Input suffix={<Icon type='search' />} />
+          </AutoComplete>
+        </div>
       </div>
     );
   }
