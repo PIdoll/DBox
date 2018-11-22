574e7b538d71b73ae1b0fdfa9c4098da7a662376
575001446@qq.com <575001446@qq.com>
2018-11-23 07:57:08 +0800

    提交anchor组件

--- components/anchor/anchorLink.jsx
+++ components/anchor/anchorLink.jsx
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 export default class AnchorLink extends React.Component {
+  state = {
+    type: 'vertical'
+  }
   static defaultProps = {
     prefixCls: 'idoll-anchor',
     href: '#',
@@ -12,12 +15,23 @@ export default class AnchorLink extends React.Component {
   }
   componentDidMount = () => {
     this.context.idollAnchor.registerLink(this.props.href);
+    if (this.context.idollAnchor.type || this.context.idollAnchor.type !== 'vertical') {
+      this.setState({
+        type: this.context.idollAnchor.type
+      })
+    }
   }
   componentWillReceiveProps(nextProps) {
     const { href } = nextProps;
+    const { type } = this.state;
     if (this.props.href !== href) {
       this.context.idollAnchor.unregisterLink(this.props.href);
       this.context.idollAnchor.registerLink(href);
+    }
+    if (type !== this.context.idollAnchor.type) {
+      this.setState({
+        type: this.context.idollAnchor.type
+      })
     }
   }
   componentWillUnmount = () => {
@@ -35,16 +49,20 @@ export default class AnchorLink extends React.Component {
   }
   render() {
     const { prefixCls, href, title, children } = this.props;
+    const { type } = this.state;
     const active = this.context.idollAnchor.activeLink === href;
-    const wrapperClassName = classNames(
+    const wrapperClassName = classNames(`${prefixCls}-link`,
       { [`${prefixCls}-link-active`]: active }
     )
+    const inlineClassName = classNames(`${prefixCls}-line-link`, {
+      [`${prefixCls}-link-active`]: active
+    });
     const titleClassName = classNames(
       `${prefixCls}-link-title`,
       { [`${prefixCls}-link-title-active`]: active, }
     )
     return (
-      <div className={wrapperClassName}>
+      <div className={type === 'vertical' || !type ? wrapperClassName : inlineClassName}>
         <a className={titleClassName} href={href} title={typeof title === 'string' ? title : ''} onClick={this.handleClick}>{title}</a>
         {children}
       </div>
--- src/templates/anchor.jsx
+++ src/templates/anchor.jsx
@@ -1,49 +1,30 @@
 import React from 'react';
 import Anchor from 'components/anchor'
 
-const Link = Anchor.Link;
+const { Link } = Anchor;
 
 export default () =>
   (
     <div id='main-container'>
-      <h1 className='h1'>有圈锚点</h1>
+      <h1 className='h1'>基本使用</h1>
       <span className='demo-anchor'>
         <Anchor>
-          <Link href='#one' title='基础模型' />
-          <Link href='#two' title='固定模型' />
-          <Link href='#three' title='API' >
-            <Link href='#four' title='锚点属性' />
-            <Link href='#five' title='链接属性' />
-          </Link>
+          <Link href='#components-anchor-demo-1' title='项目一' />
+          <Link href='#components-anchor-demo-2' title='项目二' />
+          <Link href='#components-anchor-demo-3' title='项目三' />
+          <Link href='#components-anchor-demo-4' title='项目四' />
+          <Link href='#components-anchor-demo-5' title='项目五' />
         </Anchor>
       </span>
-      <dr />
-      <h1 className='h1'>无圈锚点</h1>
-      <span>
-        <Anchor affix={false}>
-          <Link href='#one' title='基础模型' />
-          <Link href='#two' title='固定模型' />
-          <Link href='#three' title='API' >
-            <Link href='#four' title='锚点属性' />
-            <Link href='#five' title='链接属性' />
-          </Link>
+      <h1 className='h1'>Tab样式的锚点</h1>
+      <div className='demo-anchor'>
+        <Anchor type='inline'>
+          <Link href='#components-anchor-demo-1' title='项目一' />
+          <Link href='#components-anchor-demo-2' title='项目二' />
+          <Link href='#components-anchor-demo-3' title='项目三' />
+          <Link href='#components-anchor-demo-4' title='项目四' />
+          <Link href='#components-anchor-demo-5' title='项目五' />
         </Anchor>
-      </span>
-
-      <div id='one' >
-        锚点一
-      </div>
-      <div id='two'>
-        锚点二
-      </div>
-      <div id='three' >
-        锚点三
-      </div>
-      <div id='four'>
-        锚点四
-      </div>
-      <div id='five'>
-        锚点四
       </div>
     </div>
   )
