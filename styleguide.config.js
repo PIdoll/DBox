3fd5928bc2a1aa4ebd766263beadc144de5e1fb2
zhangbin <zhgbin005@gmail.com>
2018-11-26 17:18:06 +0800

    finish the breadcrumb and affix components

--- styleguide.config.js
+++ styleguide.config.js
@@ -181,10 +181,10 @@ module.exports = {
           codeSamples: 'hide',
           propsMethods: 'hide',
           components: () => ([
-         //   path.resolve(__dirname, './components/affix/affix.jsx'),
+            path.resolve(__dirname, './components/affix/index.jsx'),
             path.resolve(__dirname, './components/dropdown/index.jsx'),
             path.resolve(__dirname, './components/pagination/index.jsx'),
-            path.resolve(__dirname, './components/breadcrumb/breadcrumb.jsx'),
+            path.resolve(__dirname, './components/breadcrumb/index.jsx'),
             path.resolve(__dirname, './components/steps/steps.jsx'),
         //    path.resolve(__dirname, './components/pagination/pagination.jsx'),
         //    path.resolve(__dirname, './components/anchor/index.jsx'),
