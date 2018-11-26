92f66d58c97e2b3f3a54da07aa65e09cb2beb68d
zhangbin <zhgbin005@gmail.com>
2018-11-26 10:42:39 +0800

    finish the slider and fix the config build question

--- components/slider/style/index.jsx
+++ components/slider/style/index.jsx
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
  import '../../../style/index.less';
  import './index.less';
  import '../../tooltip/style';
 
-=======
-import './index.less'
-import '../../../style/index.less';
->>>>>>> 0f4f22a09dacf95a7cb395a6cff778788e45834a
