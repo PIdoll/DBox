1defc071eee086592d7c787194632815b0f584a3
hui5078 <839265799@qq.com>
2018-11-27 22:55:58 +0800

    update

--- src/templates/menu.jsx
+++ src/templates/menu.jsx
@@ -97,20 +97,17 @@ export default class MenuDemo extends React.Component {
             checkedChildren='浅色'
             unCheckedChildren='深色'
           />
+          <Switch onChange={this.changeMode} type='primary'>缩起菜单</Switch>
           <Menu
             theme={this.state.theme}
             onClick={this.handleClick}
             defaultOpenKeys={['sub1']}
+            inlineCollapsed={this.state.collapsed}
+            mode={this.state.mode}
             selectedKeys={[this.state.current]}
-            mode='inline'
           >
-            <SubMenu key='sub1' title={<div><Icon type='home' /><span>导航一</span></div>}>
-              <Menu.Item key='1'>子菜单一</Menu.Item>
-              <Menu.Item key='2'>子菜单二</Menu.Item>
-              <Menu.Item key='3'>子菜单三</Menu.Item>
-              <Menu.Item key='4'>子菜单四</Menu.Item>
-            </SubMenu>
-            <SubMenu title={<div><Icon type='bars' /><span>导航二</span></div>}>
+            <Menu.Item key='sub1' title='首页'><div><Icon type='home' /><span>首页</span></div></Menu.Item>
+            <SubMenu title={<div><Icon type='platform' /><span>工作台</span></div>}>
               <SubMenu title='菜单组一'>
                 <Menu.Item key='5'>子菜单五</Menu.Item>
                 <Menu.Item key='6'>子菜单六</Menu.Item>
@@ -120,11 +117,17 @@ export default class MenuDemo extends React.Component {
                 <Menu.Item key='8'>子菜单八</Menu.Item>
               </SubMenu>
             </SubMenu>
-            <SubMenu title={<div><Icon type='tool' /><span>导航三</span></div>}>
+            <SubMenu title={<div><Icon type='bars' /><span>订单中心</span></div>}>
               <Menu.Item key='9'>子菜单九</Menu.Item>
               <Menu.Item key='10'>子菜单十</Menu.Item>
               <Menu.Item key='11'>子菜单十一</Menu.Item>
               <Menu.Item key='12'>子菜单十二</Menu.Item>
+            </SubMenu>
+            <SubMenu key='sub4' title={<div><Icon type='tool' /><span>配置管理</span></div>}>
+              <Menu.Item>子菜单九</Menu.Item>
+              <Menu.Item>子菜单十</Menu.Item>
+              <Menu.Item>子菜单十一</Menu.Item>
+              <Menu.Item>子菜单十二</Menu.Item>
             </SubMenu>
           </Menu>
         </div>
