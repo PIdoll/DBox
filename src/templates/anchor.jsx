c944f0bdbf590d0b0de7d8eb2a8c0b11a4082b84
575001446@qq.com <575001446@qq.com>
2018-11-30 17:04:42 +0800

    完成anchor组件

--- src/templates/anchor.jsx
+++ src/templates/anchor.jsx
@@ -25,7 +25,6 @@ const handleClick = (e, link) => {
 };
 
 export default class AnchorView extends React.Component {
-
   handleGetCOntainer = () => {
     const DOMcontainer = this.container
     return DOMcontainer;
@@ -33,36 +32,36 @@ export default class AnchorView extends React.Component {
   render() {
     return (
       <div id='main-container' style={{ backgroundColor: '#ccc' }}>
-        {/* <h1 className='h1'>Tab样式的锚点</h1>
-      <div className='anchor' style={anchorStyle}>
-        <Anchor type='inline'>
-          <Link href='#components-anchor-demo-1_1' title='项目一' />
-          <Link href='#components-anchor-demo-2_2' title='项目二' />
-          <Link href='#components-anchor-demo-3_3' title='项目三' />
-        </Anchor>
-        <div id='components-anchor-demo-1_1'>
-          <div style={titleStyle}>项目一</div>
-          <div style={contentStyle}>这里是内容...</div>
+        <h1 className='h1'>Tab样式的锚点</h1>
+        <div className='anchor' style={anchorStyle}>
+          <Anchor type='inline'>
+            <Link href='#components-anchor-demo-1_1' title='项目一' />
+            <Link href='#components-anchor-demo-2_2' title='项目二' />
+            <Link href='#components-anchor-demo-3_3' title='项目三' />
+          </Anchor>
+          <div id='components-anchor-demo-1_1'>
+            <div style={titleStyle}>项目一</div>
+            <div style={contentStyle}>这里是内容...</div>
+          </div>
+          <div id='components-anchor-demo-2_2'>
+            <div style={titleStyle}>项目二</div>
+            <div style={contentStyle}>这里是内容...</div>
+          </div>
+          <div id='components-anchor-demo-3_3'>
+            <div style={titleStyle}>项目三</div>
+            <div style={contentStyle}>这里是内容...</div>
+          </div>
         </div>
-        <div id='components-anchor-demo-2_2'>
-          <div style={titleStyle}>项目二</div>
-          <div style={contentStyle}>这里是内容...</div>
+        <h1 className='h1'>基本使用</h1>
+        <div className='demo-anchor'>
+          <Anchor onClick={handleClick}>
+            <Link href='#components-anchor-demo-1' title='项目一' />
+            <Link href='#components-anchor-demo-2' title='项目二' />
+            <Link href='#components-anchor-demo-3' title='项目三' />
+            <Link href='#components-anchor-demo-4' title='项目四' />
+            <Link href='#components-anchor-demo-5' title='项目五' />
+          </Anchor>
         </div>
-        <div id='components-anchor-demo-3_3'>
-          <div style={titleStyle}>项目三</div>
-          <div style={contentStyle}>这里是内容...</div>
-        </div>
-      </div>
-      <h1 className='h1'>基本使用</h1>
-      <div className='demo-anchor'>
-        <Anchor onClick={handleClick}>
-          <Link href='#components-anchor-demo-1' title='项目一' />
-          <Link href='#components-anchor-demo-2' title='项目二' />
-          <Link href='#components-anchor-demo-3' title='项目三' />
-          <Link href='#components-anchor-demo-4' title='项目四' />
-          <Link href='#components-anchor-demo-5' title='项目五' />
-        </Anchor>
-      </div> */}
         <h1 className='h1'>书签类型</h1>
         <div className='demo-anchor' style={{ display: 'flex', width: 700, height: 800 }}>
           <Anchor type='bookmark' getContainer={() => this.handleGetCOntainer}>
