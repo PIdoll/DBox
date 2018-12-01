1abd30dfa54528dcaab0489c4e3d9d39d4c500ec
575001446@qq.com <575001446@qq.com>
2018-12-01 14:46:05 +0800

    提交日历组件

---
+++ src/templates/calendar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Calendar from 'components/calendar/index';
+
+export default class CalendarView extends React.Component {
+  render () {
+    return (
+      <div id='main-container'>
+        <h1 className='h1'>基本用法</h1>
+      </div>
+    )
+  }
+}
