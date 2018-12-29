a234c2d3cab238d27351f864a68d1db60c83c98a
zhangbin <zhgbin005@gmail.com>
2018-12-29 08:38:38 +0800

    fix the navbar

--- components/color/shadowColor.jsx
+++ components/color/shadowColor.jsx
@@ -3,16 +3,16 @@ import { Row, Col } from '../grid';
 import './style';
 
 const shadowColor = [
-  { title: 'Small', value: '0 8px 16px 0', description: '用于主要操作和按钮，链接，重要信息突出显示、以及进度指示和身份验证等场景。', bgColor: '#ffffff', color: '#000000', shadow: '0px 2px 8px 0px rgba(0,0,0,0.1)', rightColor: 'rgba(0,0,0,0.6)' },
-  { title: 'Large', value: '0 8px 16px 0', description: '用于主要操作和按钮，链接，重要信息突出显示、以及进度指示和身份验证等场景。', bgColor: '#ffffff', color: '#000000', shadow: '0px 8px 16px 0px rgba(0,0,0,0.1)', rightColor: 'rgba(0,0,0,0.6)' },
-  { title: 'Button', value: '0 2px 8px 0', description: '用于主要操作和按钮，链接，重要信息突出显示、以及进度指示和身份验证等场景。', bgColor: '#13B886', color: '#ffffff', shadow: '0px 2px 8px 0px rgba(19,184,134,0.2)' },
+  { title: 'Small', value: '0 8px 16px 0', description: '用于Select、Dropdown的下拉框等信息层级较低的需要用阴影的内容。', bgColor: '#ffffff', color: '#000000', shadow: '0px 2px 8px 0px rgba(0,0,0,0.1)', rightColor: 'rgba(0,0,0,0.6)' },
+  { title: 'Large', value: '0 8px 16px 0', description: '用于Alert、Message、Popconfirm、Popover、Tooltip等信息层级较高的需要用阴影的内容。', bgColor: '#ffffff', color: '#000000', shadow: '0px 8px 16px 0px rgba(0,0,0,0.1)', rightColor: 'rgba(0,0,0,0.6)' },
+  { title: 'Button', value: '0 2px 8px 0', description: '用于 Primary Button 不被卡片包裹单独使用时。', bgColor: '#13B886', color: '#ffffff', shadow: '0px 2px 8px 0px rgba(19,184,134,0.2)' },
 ]
 class ShadowColor extends React.Component {
   render() {
     return (
       <React.Fragment>
         <h2 className='idoll-color-h3'>阴影（Shadows）</h2>
-        <p className='idoll-color-p'>DBox 的投影规范了 3 种场景，Small、Large、Button，根据实际情况分别取用，基本能满足所有场景。为减少样式冗余，没有按方向细分。</p>
+        <p className='idoll-color-p'>DBox 的影规范了 3 种场景，Small、Large、Button，根据实际情况分别取用，基本能满足所有场景。为减少样式冗余，没有按方向细分。</p>
         <Row gutter={16}>
           {shadowColor.map((item, index) => {
             return (
--- styleguide/components/styleGuide.js
+++ styleguide/components/styleGuide.js
@@ -70,6 +70,7 @@ const styles = ({ color, font, base, light, link, mq }) => ({
       color: 'rgba(0,0,0,.6)',
       width: '80px',
       textAlign: 'center',
+
 		},
 	'&:hover, &:active': {
       color: '#13B886',
@@ -77,26 +78,22 @@ const styles = ({ color, font, base, light, link, mq }) => ({
       width: '80px',
       cursor: 'pointer',
       height: '61px',
-      padding: '0 12px',
       display: 'inline-block',
       borderBottom: '3px solid #13B886',
 		},
   },
-  active: {
-    color: '#13B886',
-    cursor: 'pointer',
-    height: '64px',
-    padding: '0 12px',
-    display: 'inline-block',
-    borderBottom: '3px solid #13B886',
-  }
 });
 
 export function StyleGuideRenderer({ classes, title, children, toc }) {
-	return (<div className={classes.root}><header className={classes.header}><div className={classes.bar}><Logo>{title}</Logo><nav className={classes.nav}><a className={classes.headerLink} href='index.html'>首页</a><a className={classes.headerLink} href='design.html'>设计原则</a><a className={classes.headerLink} href='doc.html'>组件文档</a><a className={classes.headerLink} href='resource.html'>设计资源</a>
-</nav></div></header><div className={classes.mainCont}><aside className={classes.sidebar}>{toc}</aside><main className={classes.content}><div className={classes.innerCont}>{children}
-</div></main>
-</div>
+const active = {
+  color: '#13B886',
+  cursor: 'pointer',
+  height: '61px',
+  display: 'inline-block',
+  borderBottom: '3px solid #13B886',
+}
+	return (<div className={classes.root}><header className={classes.header}><div className={classes.bar}><Logo>{title}</Logo>
+  <nav className={classes.nav}><a className={classes.headerLink} href='index.html'>首页</a><a className={classes.headerLink} href='design.html'>设计原则</a><a className={classes.headerLink} href='doc.html' style={active}>组件文档</a><a className={classes.headerLink} href='resource.html'>设计资源</a></nav></div></header><div className={classes.mainCont}><aside className={classes.sidebar}>{toc}</aside><main className={classes.content}><div className={classes.innerCont}>{children}</div></main></div>
 </div>
 	);
 }
