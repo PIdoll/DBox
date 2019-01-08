24169f57278f968014b5b69f415530c2fd544751
zhangbin <zhgbin005@gmail.com>
2019-01-08 13:50:18 +0800

    modify the steps affix API and UI bug

--- components/steps/readme.md
+++ components/steps/readme.md
@@ -78,7 +78,7 @@ class StepsView extends React.Component {
 
 #### **可步骤切换**
 ```jsx
-import { Steps, Button } from 'components';
+import { Steps, Button, Message } from 'components';
 
 const Step = Steps.Step;
 const steps = [
@@ -209,7 +209,7 @@ class StepsView extends React.Component {
 | labelPlacement | 指定标签放置位置，可选（vertical）放图标放图标下面 | string | horizontal |
 | progressDot |  |  |  |
 | size | 指定大小，可选（default 、small）| string | default |
-| status | 指定当前步骤的状态 | string | process |
+| status | 指定当前步骤的状态,可选wait、process、finish、error | string | process |
 | initial | 起始序号，从0开始计数 | number| 0 |
 
 #### **Steps.Step**
--- src/templates/affix.jsx
+++ src/templates/affix.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import Affix from 'components/affix';
 import Button from 'components/button';
 
@@ -15,11 +14,14 @@ export default class Demo extends React.Component {
         <Affix offsetBottom={0}>
           <Button type='primary'>固定在底部</Button>
         </Affix>
+
+
         <h1 className='h1'>固定状态改变的回调</h1>
         <Affix offsetTop={120} onChange={affixed => console.log(affixed)}>
           <Button>在距离顶部120px的地方固定住</Button>
         </Affix>
-        <h1 className='h1'>滚动容器</h1>
+
+        <h1 className='h1'>  在容器中固定</h1>
         <div className='scrollable-container' ref={node => { this.container = node }}>
           <div className='background'>
             <Affix target={() => (this.container)} offsetTop={20}>
