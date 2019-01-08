1d68608e0f8e612c10c4bb98e5c8f1373a011892
575001446@qq.com <575001446@qq.com>
2019-01-08 18:14:39 +0800

    Transfer function test

--- components/transfer/readme.md
+++ components/transfer/readme.md
@@ -96,7 +96,6 @@ class TransferView extends React.Component {
 | dataSource | 数据源，其中的数据将会被渲染到左边一栏中，targetKeys 中指定的除外。 | TransferItem[] | [] |
 | targetKeys | 显示在右侧框数据的key集合 | string[] | [] |
 | disabled | 是否禁用 | boolean | false |
-| footer | 底部渲染函数 | (props): ReactNode |  |
 | lazy | Transfer 使用了 react-lazy-load 优化性能，这里可以设置相关参数。设为 false 可以关闭懒加载。 | object , boolean | { height: 32, offset: 32 } |
 | listStyle | 两个穿梭框的自定义样式 | object |  |
 | render | 每行数据渲染函数，该函数的入参为 dataSource 中的项，返回值为 ReactElement。或者返回一个普通对象，其中 label 字段为 ReactElement，value 字段为 title | Function(record) |  |
@@ -105,6 +104,7 @@ class TransferView extends React.Component {
 | onChange | 选项在两栏之间转移时的回调函数 | (targetKeys, direction, moveKeys): void |  |
 | onScroll | 选项列表滚动时的回调函数 | (direction, event): void |  |
 | onSelectChange | 选中项发生改变时的回调函数 | (sourceSelectedKeys, targetSelectedKeys): void |  |
+| operations | 操作文案集合，顺序从上至下 | string[] | ['>', '<'] |
 
 #### **注意**
 按照 React 的规范，所有的组件数组必须绑定 key。在 Transfer 中，dataSource里的数据值需要指定 key 值。对于 dataSource 默认将每列数据的 key 属性作为唯一的标识。
--- src/templates/transfer.jsx
+++ src/templates/transfer.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Transfer from 'components/transfer/index';
 import Switch from '../../components/switch/index';
 
+import './transfer.less';
+
 const mockData = [];
 for (let i = 0; i < 20; i++) {
   mockData.push({
@@ -49,9 +51,14 @@ export default class TransferView extends React.Component {
   };
   render() {
     const { targetKeys, selectedKeys, disabled } = this.state;
+    const lazy = {height: 80, offset: 900};
+    const listStyle = {
+      width: 300
+    }
     return (
       <div id='main-container'>
         <h1 className='h1'>基本使用</h1>
+        <h2 className='h2'>最基本的用法，展示了disabled、titles、 dataSource、targetKeys、每行的渲染函数 render 以及回调函数 onChange onSelectChange onScroll 的用法。</h2>
         <div style={{marginBottom: 15}}>
           <Switch
             checked={disabled}
@@ -62,6 +69,63 @@ export default class TransferView extends React.Component {
           titles={['显示字段', '隐藏字段']}
           dataSource={mockData}
           targetKeys={targetKeys}
+          selectedKeys={selectedKeys}
+          onChange={this.handleChange}
+          onSelectChange={this.handleSelectChange}
+          onScroll={this.handleScroll}
+          render={item => item.title}
+          disabled={!disabled}
+          />
+        <h1 className='h1'>自定义类</h1>
+        <h2 className='h2'>可自定义类，控制容器的样式</h2>
+        <Transfer
+          className='container-style'
+          titles={['显示字段', '隐藏字段']}
+          dataSource={mockData}
+          targetKeys={targetKeys}
+          selectedKeys={selectedKeys}
+          onChange={this.handleChange}
+          onSelectChange={this.handleSelectChange}
+          onScroll={this.handleScroll}
+          render={item => item.title}
+          disabled={!disabled}
+          />
+        <h1 className='h1'>懒加载</h1>
+        <h2 className='h2'>Transfer 使用了 react-lazy-load 优化性能，这里可以设置相关参数。设为 false 可以关闭懒加载。</h2>
+        <Transfer
+          titles={['显示字段', '隐藏字段']}
+          dataSource={mockData}
+          lazy={lazy}
+          targetKeys={targetKeys}
+          selectedKeys={selectedKeys}
+          onChange={this.handleChange}
+          onSelectChange={this.handleSelectChange}
+          onScroll={this.handleScroll}
+          render={item => item.title}
+          disabled={!disabled}
+          />
+        <h1 className='h1'>自定义两个穿梭框的样式</h1>
+        <h2 className='h2'>如果默认的穿梭框的样式不能满足UI，则通过listStyle配置</h2>
+        <Transfer
+          titles={['显示字段', '隐藏字段']}
+          dataSource={mockData}
+          targetKeys={targetKeys}
+          listStyle={listStyle}
+          selectedKeys={selectedKeys}
+          onChange={this.handleChange}
+          onSelectChange={this.handleSelectChange}
+          onScroll={this.handleScroll}
+          render={item => item.title}
+          disabled={!disabled}
+          />
+        <h1 className='h1'>自定义操作按钮文字</h1>
+        <h2 className='h2'>默认组件的操作按钮时左右箭头，如果需要，可通过operations自定义内容，顺序从上而下</h2>
+        <Transfer
+          titles={['显示字段', '隐藏字段']}
+          operations={['右', '左']}
+          dataSource={mockData}
+          targetKeys={targetKeys}
+          listStyle={listStyle}
           selectedKeys={selectedKeys}
           onChange={this.handleChange}
           onSelectChange={this.handleSelectChange}
