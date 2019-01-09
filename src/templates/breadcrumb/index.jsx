6fba117e1c21b03c79d23f5814d150c8157698e0
zhangbin <zhgbin005@gmail.com>
2019-01-09 19:36:37 +0800

    plan to finish the inputNumber

---
+++ src/templates/breadcrumb/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import BasicBreadcrumb from './BasicBreadcrumb';
+import CustomizeBreadcrumb from './CustomizeBreadcrumb';
+import IconBreadcrumb from './IconBreadcrumb';
+import RoutetBreadcrumb from './RouterBreadcrumb';
+
+export default class Breadcrumb extends React.Component {
+  render () {
+    return (
+      <div id='main-container' className='demo-affix'>
+        <h1 className='h1'>基本面板屑</h1>
+        <h2 className='h2'>
+          测试场景:<br />
+        </h2>
+        <BasicBreadcrumb />
+        <h1 className='h1'>自定义分隔符面包屑</h1>
+        <h2 className='h2'>
+          测试场景:<br />
+        </h2>
+        <CustomizeBreadcrumb />
+        <h1 className='h1'>带图标的面包屑</h1>
+        <h2 className='h2'>
+          测试场景:<br />
+        </h2>
+        <IconBreadcrumb />
+        <h1 className='h1'>React-Router4.0路由</h1>
+        <h2 className='h2'>
+          测试场景:<br />
+        </h2>
+        <RoutetBreadcrumb />
+      </div>
+    )
+  }
+}
