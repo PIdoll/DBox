8cb4d489b63fd8818fd0700b4c84c60a452e84b1
hui5078 <839265799@qq.com>
2019-01-16 11:27:46 +0800

    tootip

--- components/tooltip/tooltip.jsx
+++ components/tooltip/tooltip.jsx
@@ -6,7 +6,7 @@ export default class Tooltip extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      visible: !!props.visible || !!props.defaultVisible,
+      visible: !!props.visible || !!props.defaultVisible || false,
     };
   }
   static defaultProps = {
