75af98b62f737fac975da2c5dc23b4245f1a780f
yangyang <575001446@qq.com>
2020-01-15 17:02:27 +0800

    更新版本记录

--- components/version/index.jsx
+++ components/version/index.jsx
@@ -17,6 +17,21 @@ class Version extends React.Component {
       <div >
         <Timeline >
           <Timeline.Item >
+            <h2>0.2.9</h2>
+            <p >2020-01-16</p>
+            <p>📢 更新 组件官网首页。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/506'>#506</a></p>
+            <p>🐞 修复 Table组件排序图标过小。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/497'>#497</a></p>
+            <p>🐞 修复 Tag组件cursor: pointer场景。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/507'>#507</a></p>
+            <p>🐞 修复 Affix组件无法固定在容器顶部。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/517'>#517</a></p>
+            <p>🐞 修复 Dropdown组件<code>disabled</code>属性。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/510'>#510</a></p>
+            <p>🐞 修复 Badge组件设置和显示数字不一致。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/498'>#498</a></p>
+            <p>🐞 修复 Steps组件竖直场景下步骤图标距离。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/511'>#511</a></p>
+            <p>🐞 修复 Calendar日历组件切换日期箭头颜色。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/500'>#500</a></p>
+            <p>💄 优化 Modal组件 confirm场景下组件样式统一。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/496'>#496</a></p>
+            <p>🛠 新增 Radio组件只读API readOnly <code>hidden</code>。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/516'>#516</a></p>
+            <p>🛠 新增 Checkbox组件只读API readOnly <code>hidden</code>。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/505'>#505</a></p>
+          </Timeline.Item>
+          <Timeline.Item >
             <h2>0.2.8</h2>
             <p >2019-12-26</p>
             <p>🐞 移除 Form <code>hideRequiredMark。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/477'>#477</a></code></p>
@@ -31,21 +46,6 @@ class Version extends React.Component {
             <p>🐞 修复 设置format设为<code>HH:mm</code> 时，时间面板秒的一列会空白一列。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/487'>#487</a></p>
             <p>🐞 修复 DatePicker组件中format属性为数组时，PropType数组校验报错。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/478'>#478</a></p>
             <p>🐞 修复 menu在禁用条件下<code>hover</code>状态不正确、顶部导航高度写死问题。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/482'>#482</a></p>
-          </Timeline.Item>
-          <Timeline.Item >
-            <h2>0.2.9</h2>
-            <p >2020-01-16</p>
-            <p>📢 更新 组件官网首页。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/506'>#506</a></p>
-            <p>🐞 修复 Table组件排序图标过小。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/497'>#497</a></p>
-            <p>🐞 修复 Tag组件cursor: pointer场景。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/507'>#507</a></p>
-            <p>🐞 修复 Affix组件无法固定在容器顶部。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/517'>#517</a></p>
-            <p>🐞 修复 Dropdown组件<code>disabled</code>属性。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/510'>#510</a></p>
-            <p>🐞 修复 Badge组件设置和显示数字不一致。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/498'>#498</a></p>
-            <p>🐞 修复 Steps组件竖直场景下步骤图标距离。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/511'>#511</a></p>
-            <p>🐞 修复 Calendar日历组件切换日期箭头颜色。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/500'>#500</a></p>
-            <p>💄 优化 Modal组件 confirm场景下组件样式统一。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/496'>#496</a></p>
-            <p>🛠 新增 Radio组件只读API readOnly <code>hidden</code>。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/516'>#516</a></p>
-            <p>🛠 新增 Checkbox组件只读API readOnly <code>hidden</code>。<a target='_blank' href='https://github.com/PIdoll/DBox/pull/505'>#505</a></p>
           </Timeline.Item>
         </Timeline>
       </div>
