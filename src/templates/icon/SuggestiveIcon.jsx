8cec0610b9a7e84d22d0b3d0704b7df29572432f
hui5078 <839265799@qq.com>
2020-03-17 15:37:46 +0800

    新增assistant、wechat、pause_circle、position图标

--- src/templates/icon/SuggestiveIcon.jsx
+++ src/templates/icon/SuggestiveIcon.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import {Icon} from 'components';
 
-const icons = ['bars', 'block', 'check', 'check-circle-o', 'check-circle', 'checkbox-blank-o', 'checkbox-checked', 'checkbox-indeterminate', 'close-circle', 'close-circle-o', 'close', 'delete', 'done',
-'error-circle-o', 'error-circle', 'filter', 'help-circle-o', 'help-circle', 'history', 'info-circle-o', 'info-circle', 'list', 'loading', 'menu', 'more', 'plus_box', 'plus-circle-o',
+const icons = ['bars', 'block', 'check', 'check-circle-o', 'check-circle', 'checkbox-blank-o', 'checkbox-checked', 'checkbox-indeterminate', 'close-circle', 'close-circle-o', 'close', 'delete', 'done', 'error-circle-o', 'error-circle', 'filter', 'help-circle-o', 'help-circle', 'history', 'info-circle-o', 'info-circle', 'list', 'loading', 'menu', 'more', 'plus_box', 'plus-circle-o',
 'plus-circle', 'plus', 'radio-checked', 'radio-unchecked', 'refresh', 'remove-circle-o', 'remove-circle', 'remove', 'search', 'warning-o', 'warning'];
 export default class SuggestiveIcon extends React.Component {
   render() {
